e9c0cfc1fc86aa9f8675bcc99c1df722e312e272
ytokarevskaya <lan2214@yandex.ru>
2020-10-18 19:50:20 +0300

    Проверено

--- 14.2.js
+++ 14.2.js
@@ -16,19 +16,20 @@ const jsonString = `
 `;
 
 const data = JSON.parse(jsonString);
-const list = data.list;
-const results = [];
+// const list = data.list;
+// const results = [];
 
-list.forEach(result => {
-  results.push({
-  name: result.name,
-  age: Number(result.age),
-  prof: result.prof
-  });
- });
-const log = {
-   list: results
-};
+// list.forEach(result => {
+//   results.push({
+//   name: result.name,
+//   age: Number(result.age),
+//   prof: result.prof
+//   });
+//  });
+// const log = {
+//    list: results
+// };
 
-console.log(log)
+console.log(data)
 
+// Решение верное, но не совсем оптимальное - много лишних действий :) JSON.parse по умолчанию создает из JSON-строчки готовый Javascript-объект, заполнять объект вручную (как при работе с XML) нет необходимости. Выше закомментировала всё лишнее
